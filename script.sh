f3c472a926b7e351c1a1ad7eddfb307aecac64a5
laocane <adamboryslawski97@gmail.com>
2023-03-08 19:15:28 +0100

    use ubuntu

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#gcloud compute instances create hpc-test-1 hpc-test-2 --zone=us-east1-d --image-family=hpc-centos-7 --image-project=cloud-hpc-image-public --maintenance-policy=TERMINATE --machine-type=c2d-standard-2 --metadata=google_install_mpi="--intel_mpi"
+gcloud compute instances create mpi-test-1 mpi-test-2 --image-family=ubuntu-2204-lts --image-project=ubuntu-os-cloud
 
 instances=$(gcloud compute instances list --project sigma-archery-378216 | grep NAME | cut -d' ' -f 2)
 for x in $instances;do
