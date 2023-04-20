23a5b1cb6141e20e3ce8bb5c7c4dfb01d649dcbc
aboryslawski <31217209+aboryslawski@users.noreply.github.com>
2023-04-20 20:20:06 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,11 @@
-gcloud compute instances create mpi-test-1 mpi-test-2 --image-family=ubuntu-2204-lts --image-project=ubuntu-os-cloud
-
-instances=$(gcloud compute instances list --project sigma-archery-378216 | grep NAME | cut -d' ' -f 2)
-for x in $instances;do
-    echo $x
+for x in $(cat ips)
+do
+        scp mat_vec_row_MPI $x:/home/xz1szybko
+        echo "executing mpi install"
+        ssh -t $x 'sudo apt install -y libopenmpi-dev; bash -l' -o "StrictHostKeyChecking no"
+        echo "mpi install complete"
 done
+
+------------------
+
+for x in $(cat target_zones);do for y in {1..8};do echo mpi-gp-$y && echo $x && gcloud compute instances create mpi-gp-$y --machine-type=e2-small --zone=$x;done;done
